87a7a726edee64c5a8ebe243c51681a0c9d27df7
Jhshshsz <89310613+Jhshshsz@users.noreply.github.com>
2022-01-07 22:55:00 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -598,7 +598,7 @@ jano99.on("ready", () => {
 
 /////////////////////
 jano100.on("ready", () => {
-  var join = jano100.channels.get(""); /// id voice
+  var join = jano100.channels.get("925066852575633468"); /// id voice
   if (join) join.join();
 });
 //////// playing or dnd  streaming ////////
@@ -2006,4 +2006,4 @@ jano96.login("");
 jano97.login("");
 jano98.login("");
 jano99.login("");
-jano100.login("");
+jano100.login("OTI4MjI1NjEyNjIyMDY5Nzcw.YdV1sQ.LXbOR7P8UzPbQo-iG4LHsVN_U7w");
